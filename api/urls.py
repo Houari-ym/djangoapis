f662b2a6b38e4dca0beeccb4773047fe7695cae9
Didin-e <houari.ymp@gmail.com>
2022-10-27 22:10:18 +0800

    chapter-11

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('todos/', views.TodoList.as_view()),
-    path('show/', views.ListAllData.as_view()),
-    path('todosU/', views.TodoListCreate.as_view()),
+    path('show/', views.ListAllData.as_view()),  # Just for play
+    path('todosU/', views.TodoListCreate.as_view()),  # just for play
     path('todos/<int:pk>', views.TodoRetrieveUpdateDestroy.as_view()),
-    path('todos/<int:pk>/complete', views.TodoToggleComplete.as_view())
+    path('todos/<int:pk>/complete', views.TodoToggleComplete.as_view()),
+    path('signup/', views.signup, name="sign-up")
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,12 @@
+from django.http import JsonResponse
+from psycopg2 import IntegrityError
 from rest_framework import generics, permissions
 from .serializers import ShowAllData, TodoSerializer, TodoToggleCompleteSerializer
 from todo.models import Todo
+from django.contrib.auth.models import User
+from rest_framework.parsers import JSONParser
+from rest_framework.authtoken.models import Token
+from django.views.decorators.csrf import csrf_exempt
 
 
 class TodoList(generics.ListAPIView):
@@ -47,10 +53,34 @@ class TodoToggleComplete(generics.UpdateAPIView):
     serializer_class = TodoToggleCompleteSerializer
     permission = [permissions.IsAuthenticated]
 
-    def get_queryset(self): 
+    def get_queryset(self):
         user = self.request.user
         return Todo.objects.filter(user=user)
 
     def perform_update(self, serializer):
         serializer.instance.completed = not(serializer.instance.completed)
         serializer.save()
+
+
+# Sign up function:
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)
+
+            # at this stage you can change between user and superuser
+            user = User.objects.create_superuser(
+                username=data['username'], email=data['email'], password=data['password'])
+
+            # user = User.objects.create_user(
+            #     username=data['username'], email=data['email'], password=data['password'])
+            user.save()
+            token = Token.objects.create(user=user)
+            # if user == User.user:
+            #     status = 201
+            # else:
+            #     status = 200
+            return JsonResponse({'token_sent_by_server_as_TokenAuth': str(token)}, status=201)
+        except IntegrityError:
+            return JsonResponse({'error': 'username already taken chanage another one!'}, status=400)
